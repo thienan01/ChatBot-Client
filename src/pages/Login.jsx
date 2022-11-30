f4f5b51a9cc01f15e1cbf363b16c7e35298120a0
Thien An Le <87899490+thienan01@users.noreply.github.com>
2022-11-30 22:46:47 +0700

    up

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,40 +1,27 @@
 import React, { useState } from "react";
 import { BASE_URL } from "../global/globalVar";
-import {
-  Container,
-  Row,
-  Col,
-  InputGroup,
-  InputGroupText,
-  Input,
-  Button,
-} from "reactstrap";
+import logo from "../assets/pngegg.png";
 import { useNavigate } from "react-router-dom";
-import "../styles/login.css";
+import "../styles/main.css";
+import "../styles/util.css";
 import { POST } from "../functionHelper/APIFunction";
 import { getCookie, setCookie } from "../functionHelper/GetSetCookie";
+import Spinner from "react-bootstrap/Spinner";
 
 const Login = () => {
   const navigate = useNavigate();
-  const [toggle, setToggle] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-  };
-
-  const handleToggle = () => {
-    setToggle(!toggle);
-  };
-
+  const [loading, setLoading] = useState(false);
   const handleLogin = () => {
+    console.log("handle");
     try {
       let body = {
         username: username,
         password: password,
       };
       POST(BASE_URL + "api/user/login", JSON.stringify(body)).then((res) => {
+        setLoading(false);
         if (res.http_status !== "OK") {
           throw res.exception_code;
         }
@@ -50,80 +37,80 @@ const Login = () => {
     navigate("/register");
   };
   return (
-    <div className="login__section shadow">
-      <Container className="login__container">
-        <Row>
-          <Col lg="6" md="6" sm="12" className="m-auto text-center centered">
-            {/* tomorow check form__container */}
-            <h2>Welcome! Join our world now</h2>
-            <form className="form mb-10" onSubmit={submitHandler}>
-              <div className="form__container">
-                <h6 className="form__label">Username:</h6>
-                <div className="form__group">
-                  <InputGroup className="Input">
-                    <InputGroupText>
-                      <i className="ri-mail-line" />
-                    </InputGroupText>
-                    <Input
-                      name="email"
-                      placeholder="Username"
-                      required
-                      onChange={(u) => setUsername(u.target.value)}
-                    />
-                  </InputGroup>
-                </div>
-                <h6 className="form__label">Password:</h6>
-                <div className="form__group">
-                  <InputGroup className="Input">
-                    <InputGroupText>
-                      <i className="ri-lock-line"></i>
-                    </InputGroupText>
-                    <Input
-                      type={toggle ? "text" : "password"}
-                      placeholder="Password"
-                      name="password"
-                      required
-                      onChange={(p) => setPassword(p.target.value)}
-                    />
-                    <Button onClick={handleToggle} color="primary">
-                      {!toggle ? (
-                        <i className="ri-eye-close-line"></i>
-                      ) : (
-                        <i className="ri-eye-line"></i>
-                      )}
-                    </Button>
-                  </InputGroup>
-                </div>
-                <InputGroup>
-                  <Input type="checkbox" />{" "}
-                  <label style={{ marginLeft: "5px" }}>Remember Me</label>
-                </InputGroup>
-                <Container className="btn__container">
-                  <Button
-                    type="submit"
-                    color="success"
-                    outline
-                    className="add__btn"
-                    onClick={handletoRegister}
-                  >
-                    Register
-                  </Button>
-                  <Button
-                    type="submit"
-                    color="danger"
-                    outline
-                    className="add__btn"
-                    onClick={handleLogin}
-                  >
-                    Login
-                  </Button>
-                </Container>
-              </div>
-            </form>
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <>
+      <div className="limiter">
+        <div className="container-login100">
+          <div className="wrap-login100">
+            <img src={logo} alt="" id="logoLogin" />
+            <span className="login100-form-title p-b-26">
+              Welcome to Chatbot service
+            </span>
+            <span className="login100-form-title p-b-48">
+              <i className="zmdi zmdi-font"></i>
+            </span>
+
+            <div
+              className="wrap-input100 validate-input"
+              data-validate="Valid email is: a@b.c"
+            >
+              <input
+                className="input100"
+                type="text"
+                name="email"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+              />
+            </div>
+
+            <div
+              className="wrap-input100 validate-input"
+              data-validate="Enter password"
+            >
+              <span className="btn-show-pass">
+                <i className="zmdi zmdi-eye"></i>
+              </span>
+              <input
+                className="input100"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+
+            <div className="wrap-login100-form-btn">
+              <div className="login100-form-bgbtn"></div>
+              <button
+                className="login100-form-btn bg-primary"
+                onClick={() => {
+                  setLoading(true);
+                  handleLogin();
+                }}
+              >
+                {loading ? (
+                  <Spinner style={{ width: "30px", height: "30px" }} />
+                ) : (
+                  "Login"
+                )}
+              </button>
+            </div>
+
+            <div className="text-center p-t-115">
+              <span className="txt1">Don’t have an account?</span>
+
+              <a className="txt2" href="#">
+                Sign Up
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
