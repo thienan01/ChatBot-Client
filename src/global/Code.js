e7024ca30086d1271c17a74de0048d3bac524273
Le Thien An <thienan12359@gmail.com>
2024-01-01 22:55:27 +0700

    chat emb

--- src/global/Code.js
+++ src/global/Code.js
@@ -6,38 +6,43 @@ function Code() {
             var currentNodeId = "_BEGIN";
     </script>
     <div class="containerChat">
-    <div class="chatbox" style="position:fixed">
-        <div class="chatbox__support">
-            <div class="chatbox__header">
-                <div class="chatbox__content--header">
-                    <h4 class="chatbox__heading--header">Chat support</h4>
-                </div>
-            </div>
-            <div class="chatbox__messages">
-                    
-                <button class="btn-rec Rec" >Recording</button>
-            </div>
-            <div class="chatbox__footer">
-                <img src="https://cdn.jsdelivr.net/gh/thienan01/new/emojis.svg" alt="">
-                <img src="https://cdn.jsdelivr.net/gh/thienan01/new/microphone.svg" alt="" onclick="recognizeVoice()">
-                <input type="text" placeholder="Write a message..." id="inputMessage">
-                <p class="chatbox__send--footer" id="btnSend" onclick="handleSendMsg()" style="margin-top:16px">Send</p>
-                <img src="https://cdn.jsdelivr.net/gh/thienan01/new/attachment.svg" alt="">
-            </div>
+    <div class="chatbox" style="position: fixed">
+      <div class="chatbox__support">
+        <div class="chatbox__header">
+          <div class="chatbox__content--header">
+            <h4 class="chatbox__heading--header">Chat support</h4>
+          </div>
         </div>
-        
-        <div class="chatbox__button">
-            <button>button</button>
+        <div class="chatbox__messages">
+          <button class="btn-rec Rec">Recording</button>
         </div>
+        <div class="chatbox__footer">
+          <img src="https://cdn.jsdelivr.net/gh/thienan01/new/emojis.svg" alt=""/>
+          <img src="https://cdn.jsdelivr.net/gh/thienan01/new/microphone.svg" alt="" onclick="recognizeVoice()"/>
+          <input type="text" placeholder="Write a message..." id="inputMessage"/>
+          <p class="chatbox__send--footer" id="btnSend" onclick="handleSendMsg()" style="margin-top: 16px">
+            Send
+          </p>
+        </div>
+      </div>
+      <div class="chatbox__button"><button>button</button></div>
     </div>
-</div>
-<link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;1,300&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/thienan01/pluggin-cdns@latest/chat.min.css">
-<script src="https://kit.fontawesome.com/6a72490e9a.js" crossorigin="anonymous"></script>
-
-<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="https://cdn.jsdelivr.net/gh/thienan01/newpluggin@latest/handleChat.min.js"></script>
-
+  </div>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,300;0,400;0,600;1,300&display=swap"
+    rel="stylesheet"/>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/gh/thienan01/ChatBoxCDNs@latest/chat.min.css"
+  />
+  <script
+    src="https://kit.fontawesome.com/6a72490e9a.js"
+    crossorigin="anonymous"
+  ></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sockjs-client/1.5.1/sockjs.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/stomp.js/2.3.3/stomp.min.js"></script>
+  <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/gh/thienan01/ChatBoxCDNs@latest/handleChatV2.min.js"></script>
     `;
 }
 export default Code;
